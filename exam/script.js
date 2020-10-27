308e708d8e4203c66a70da056642e88b0a65233d
DevInformation <44543774+DevInfomation@users.noreply.github.com>
2020-10-27 19:01:25 +0100

    Update script.js

--- exam/script.js
+++ exam/script.js
@@ -1,1 +1,14 @@
+let surname = "Milkovski"
+console.log(surname);
 
+let name = "Stefan";
+console.log(name);
+
+let studentID = 4220;
+console.log(studentID);
+
+let eMail = "stefanmilkovski99@gmail.com";
+console.log(eMail);
+
+let note ="Note"
+console.log(note);
